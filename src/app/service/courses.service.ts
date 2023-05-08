966f7764f9033d368bd41d9f759b675e31121c6b
uzpsychologist@gmail.com <uzpsychologist@gmail.com>
2023-05-08 11:27:40 +0500

    new features added

--- src/app/service/courses.service.ts
+++ src/app/service/courses.service.ts
@@ -11,6 +11,13 @@ export class CoursesService {
 
   constructor(private http: HttpClient) {}
 
+  addCourse(course: Course): Observable<Course> {
+    return this.http.post<Course>(this.coursesUrl, course);
+  }
+  updateCourse(course: Course): Observable<any> {
+    const url = `${this.coursesUrl}/${course.id}`;
+    return this.http.put(url, course);
+  }
   getCourses(): Observable<Course[]> {
     return this.http.get<Course[]>(this.coursesUrl);
   }
