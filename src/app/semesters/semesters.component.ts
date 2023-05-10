d91ffecb6c78da63a16ea405a647227a8b0fd3fc
uzpsychologist@gmail.com <uzpsychologist@gmail.com>
2023-05-10 18:18:32 +0500

    student component working create, update needs a bit work

--- src/app/semesters/semesters.component.ts
+++ src/app/semesters/semesters.component.ts
@@ -9,11 +9,31 @@ import {SemestersService} from "../service/semesters.service";
 })
 export class SemestersComponent implements OnInit {
   semesters: Semester[];
-
+  newSemester: Semester = new Semester();  // Object to hold form data
+  updatedSemester: Semester = new Semester();
   constructor(private semestersService: SemestersService) { }
 
   ngOnInit() {
     this.getSemesters();
+  }
+
+  addSemester(): void {
+    this.semestersService.addSemester(this.newSemester).subscribe(semester => {
+      this.semesters.push(semester);
+      this.newSemester = new Semester();
+    });
+  }
+  updateSemester(): void {
+    this.semestersService.updateSemester(this.updatedSemester).subscribe(semester => {
+      const index = this.semesters.findIndex(s => s.id === semester.id);
+      if (index > -1) {
+        this.semesters[index] = semester;
+      }
+      this.updatedSemester = new Semester();
+    });
+  }
+  selectSemester(semester: Semester): void {
+    this.updatedSemester = { ...semester };  // Create a copy to avoid mutating the original object
   }
 
   getSemesters(): void {
