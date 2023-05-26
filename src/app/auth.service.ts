7138030fbf41aa0806deda2c919582018b5d29d2
Gulomjon Sulaymonov <uzpsychologist@gmail.com>
2023-05-26 23:42:39 +0500

    insturcter works

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,20 +1,23 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import {User} from "./User";
-
+import { Router } from '@angular/router';
+import jwt_decode from 'jwt-decode';
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
 
   private users: User[] = JSON.parse(localStorage.getItem('users') || '[]');
-
+  constructor(private router: Router) {}
   register(user: User): Observable<User> {
     // Check if a user with the same Neptun Code already exists.
+/*
     const existingUser = this.users.find(u => u.neptunCode === user.neptunCode);
     if (existingUser) {
       throw new Error('A user with this Neptun Code already exists.');
     }
+*/
 
     // Add the new user and save the users array back to local storage.
     this.users.push(user);
@@ -26,7 +29,7 @@ export class AuthService {
 
   login(username: string, password: string): Observable<string> {
     const users: User[] = JSON.parse(localStorage.getItem('users') || '[]');
-    const user = users.find(u => u.neptunCode === username && u.password === password);
+    const user = users.find(u => u.name === username && u.password === password);
 
     if ((username === 'user' && password === 'password') || user) {
       const header = {
@@ -41,17 +44,19 @@ export class AuthService {
 
       const base64UrlHeader = btoa(JSON.stringify(header));
       const base64UrlPayload = btoa(JSON.stringify(payload));
-      const fakeToken = `${base64UrlHeader}.${base64UrlPayload}.fake-signature`;
-
+      const fakeToken = `${base64UrlHeader}.${base64UrlPayload}`;
       localStorage.setItem('token', fakeToken);
+      console.log(jwt_decode(fakeToken))
       return of(fakeToken);
     } else {
       return of(null);
     }
   }
 
+
   logout(): void {
     localStorage.removeItem('token');
+    this.router.navigate(['/login']);
   }
   isLoggedIn(): boolean {
     const token = localStorage.getItem('token');
